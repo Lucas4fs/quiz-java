e599ea8db0cff6c352deb0572144518b3aa259af
Lucas Fonseca <96666996+Lucas4fs@users.noreply.github.com>
2023-11-03 16:43:47 -0300

    Update Quiz.java

--- src/com/example/quiz/Quiz.java
+++ src/com/example/quiz/Quiz.java
@@ -2,13 +2,13 @@ package com.example.quiz;
 
 public class Quiz
 {
-	String qstOne =
-	"\n1 - Java È uma linguagem:\n\n"
-	+ "A - Altamente tipada e orientada a objetos.\n" //RESP
-	+ "B - N„o tipada e n„o orientada a objetos.\n"
-	+ "C - Altamente tipada, orientada a objetos e n„o possui classes prontas.\n"
+    String qstOne =
+    "\n1 - Java √© uma linguagem:\n\n"
+    + "A - Altamente tipada e orientada a objetos.\n" //RESP
+    + "B - N√£o tipada e n√£o orientada a objetos.\n"
+    + "C - Altamente tipada, orientada a objetos e n√£o possui classes prontas.\n"
     + "D - Interpretada.\n"
-    + "E - Interpretada, de tipagem din‚mica e multiparadigma.\n";
+    + "E - Interpretada, de tipagem din√¢mica e multiparadigma.\n";
 
     public void printQstOne()
     {
@@ -16,12 +16,12 @@ public class Quiz
     }
     
     String qstTwo =
-    "\n2 - Em Java, palavras-chave como public, protected e private s„o modificadoras de acesso ou de visibilidade e podem ser utilizadas em classes e seus atributos e mÈtodos para definir acessos mais ou menos restritos a esses elementos. Na ordem de menos restrito para mais restrito, os tipos de acesso que essas palavras-chave (ou a ausÍncia delas) podem definir s„o:\n"
-    + "A - P˙blico, padr„o, protegido e privado.\n"
-    + "B - Amigo, p˙blico, padr„o e privado.\n"
-    + "C - P˙blico, amigo, protegido e privado.\n"
-    + "D - P˙blico, protegido, padr„o e privado.\n" //RESP
-    + "E - Padr„o, p˙blico, privado e protegido.\n";
+    "\n2 - Em Java, palavras-chave como public, protected e private s√£o modificadoras de acesso ou de visibilidade e podem ser utilizadas em classes e seus atributos e m√©todos para definir acessos mais ou menos restritos a esses elementos. Na ordem de menos restrito para mais restrito, os tipos de acesso que essas palavras-chave (ou a aus√™ncia delas) podem definir s√£o:\n"
+    + "A - P√∫blico, padr√£o, protegido e privado.\n"
+    + "B - Amigo, p√∫blico, padr√£o e privado.\n"
+    + "C - P√∫blico, amigo, protegido e privado.\n"
+    + "D - P√∫blico, protegido, padr√£o e privado.\n" //RESP
+    + "E - Padr√£o, p√∫blico, privado e protegido.\n";
     
     public void printQstTwo()
     {
@@ -29,12 +29,12 @@ public class Quiz
     }
     
     String qstThree = 
-    "\n3 - Sobre a linguagem de programaÁ„o Java, È correto afirmar:\n"
-    + "A - Possibilita o uso de heranÁa simples e m˙ltipla.\n"
-    + "B - Utiliza o processo de garbage collector para remover trechos de cÛdigo que n„o sejam utilizados.\n" //RESP
-    + "C - Faz uso da lÛgica de ponteiros para o armazenamento de informaÁıes.\n"
+    "\n3 - Sobre a linguagem de programa√ß√£o Java, √© correto afirmar:\n"
+    + "A - Possibilita o uso de heran√ßa simples e m√∫ltipla.\n"
+    + "B - Utiliza o processo de garbage collector para remover trechos de c√≥digo que n√£o sejam utilizados.\n" //RESP
+    + "C - Faz uso da l√≥gica de ponteiros para o armazenamento de informa√ß√µes.\n"
     + "D - Adota o conceito de multithreading.\n"
-    + "E - Java Virtual Machine (JVM) È um software que emula diversos sistemas operacionais.\n";
+    + "E - Java Virtual Machine (JVM) √© um software que emula diversos sistemas operacionais.\n";
     	    
     public void printQstThree()
     {
@@ -42,7 +42,7 @@ public class Quiz
     }
     	    
     String qstFour =
-    "\n4 - Marcos est· trabalhando em um projeto de sistemas em Java e quer garantir que o atributo de uma classe seja inicializado apenas uma vez e que seu valor n„o possa ser modificado posteriormente. Na linguagem de programaÁ„o Java, esse atributo deve ser definido com o modificador:\n"
+    "\n4 - Marcos est√° trabalhando em um projeto de sistemas em Java e quer garantir que o atributo de uma classe seja inicializado apenas uma vez e que seu valor n√£o possa ser modificado posteriormente. Na linguagem de programa√ß√£o Java, esse atributo deve ser definido com o modificador:\n"
     + "A - Abstract.\n"
     + "B - Default.\n"
     + "C - Final.\n" //RESP
@@ -55,7 +55,7 @@ public class Quiz
     }
     	    	    
     String qstFive =
-    "\n5 - Qual comando È respons·vel por criar uma inst‚ncia de uma classe?\n"
+    "\n5 - Qual comando √© respons√°vel por criar uma inst√¢ncia de uma classe?\n"
     + "A - main.\n"
     + "B - public.\n"
     + "C - static.\n"
